2235cc30f9f62df87521272506ee87715fe049b8
JeongHun <sjh960612@naver.com>
2024-08-27 16:06:06 +0900

    JeongHun
    9020번 골드바흐의 추측

--- BaekJoon/02. Sliver/9020.cpp
+++ BaekJoon/02. Sliver/9020.cpp
@@ -1,11 +1,54 @@
 // # ∞ÒµÂπŸ»Â¿« √ﬂ√¯
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+void Eratos(vector<bool>& vecPrime)
+{
+	for (int i = 2; i < vecPrime.size(); i++)
+	{
+		vecPrime[i] = true;
+	}
+
+	for (int i = 2; i * i < vecPrime.size(); i++)
+	{
+		if (!vecPrime[i]) continue;
+
+		for (int j = i * i; j < vecPrime.size(); j += i)
+		{
+			vecPrime[j] = false;
+		}
+	}
+}
+
 int main()
 {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	vector<bool> vecPrime(10001);
+	Eratos(vecPrime);
+
+	int T;
+	cin >> T;
+
+	int N;
+	while (T--)
+	{
+		cin >> N;
+
+		for (int i = N / 2; i >= 2; i--)
+		{
+			if (vecPrime[i] && vecPrime[N - i])
+			{
+				cout << i << " " << N - i << '\n';
+				break;
+			}
+		}
+	}
 
 	return 0;
 }
