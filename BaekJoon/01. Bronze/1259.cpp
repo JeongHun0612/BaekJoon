135eae01ce5d23f9b3164ceebab8a2780ade72e0
JeongHun <sjh960612@naver.com>
2024-08-30 15:14:04 +0900

    JeongHun
    1747번. 소수&팰린드롬

---
+++ BaekJoon/01. Bronze/1259.cpp
@@ -0,0 +1,33 @@
+// # ÆÓ¸°µå·Ò¼ö
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(int num)
+{
+	string str = to_string(num);
+
+	for (int i = 0; i < str.length() / 2; i++)
+	{
+		if (str[i] != str[str.length() - 1 - i]) return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int num;
+	while (cin >> num)
+	{
+		if (num == 0) break;
+		cout << (isPalindrome(num) ? "yes" : "no") << '\n';
+	}
+
+	return 0;
+}
---
+++ BaekJoon/02. Sliver/1747.cpp
@@ -0,0 +1,53 @@
+// # 소수 & 팰린드롬
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+#define MAX 1003002
+
+using namespace std;
+
+bool isPalindrome(int num)
+{
+	const string str = move(to_string(num));
+
+	for (int i = 0; i < str.length() / 2; i++)
+	{
+		if (str[i] != str[str.length() - 1 - i]) return false;
+	}
+
+	return true;
+}
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	vector<bool> vecB(MAX);
+	vector<int> vecI;
+
+	for (int i = 2; i < MAX; i++)
+	{
+		if (vecB[i]) continue;
+
+		if (isPalindrome(i))
+		{
+			vecI.emplace_back(i);
+		}
+
+		for (int j = i * 2; j < MAX; j += i)
+		{
+			vecB[j] = true;
+		}
+	}
+
+	int N;
+	cin >> N;
+
+	cout << *(lower_bound(vecI.begin(), vecI.end(), N)) << '\n';
+
+	return 0;
+}
